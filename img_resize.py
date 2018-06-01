462c35d52f42aa99bba9cceae3c74a5db240cef6
shubham <shubhamgupta20041998@gmail.com>
2018-06-01 14:12:12 +0530

    Update img_resize.py

--- img_resize.py
+++ img_resize.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python
+#importing pillow library
 from PIL import Image
+#importing os and system
 import os, sys
-
+#defining path of the folder to be resized
 path = "D:\projects\projects\cat_image"
+#openind and getting all the content of the file
 dirs = os.listdir( path )
 for item in dirs:
     if os.path.isfile(path+item):
+        #opening image
         im = Image.open(path+item)
         f, e = os.path.splitext(path+item)
+        #resizing in 64x64 size
         imResize = im.resize((64,64), Image.ANTIALIAS)
+        #saving image 
         imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
 
 
