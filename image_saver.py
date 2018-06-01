ee1998b3b94bce8b7687b88d578eecdcf22518b2
shubham <shubhamgupta20041998@gmail.com>
2018-06-01 14:09:42 +0530

    Update image_saver.py

--- image_saver.py
+++ image_saver.py
@@ -1,11 +1,16 @@
+# importing urllib.request library
 import urllib.request
 
 i=0
+#opening file of url list downloaded by imagenet
 with open("imagenet_cats.txt") as f:
+# reading file
     for line in f:
         if line[0]=='h':
             i=str(i)
+            #i use try because many urls are either defective or are not present now so it will skip those urls
             try:
+                #opening and saving image in the current folder as integer names
                 urllib.request.urlretrieve(line, i+".jpg")
             except Exception as e:
                 print(e)
